ab06d0aab55e767ef2b20f68517f68a0fc12aee3
NPC <npc@npcdembp.home>
2021-07-15 21:21:28 +0200

    pc screen modified

--- components/PC/PCScreen.js
+++ components/PC/PCScreen.js
@@ -1,7 +1,35 @@
 import Image from "next/image";
+import pcBg from "../../public/pc-background.png";
+import qrcode from "../../public/qrcode.png";
+
+import classes from "./PCScreen.module.scss";
 
 const PCScreen = () => {
-  return <Image src="/qrcode.png" layout="fill" objectFit="contain" />;
+  return (
+    <div className={classes.screen}>
+      <Image
+        className={classes.bg}
+        src={pcBg}
+        layout="fill"
+        objectFit="cover"
+        placeholder="blur"
+      />
+
+      <div className={classes.text}>
+        <h1>爱他美代购培训证书</h1>
+        <h2>培训验证通道</h2>
+        <div className={classes.qrcode}>
+          <Image
+            src={qrcode}
+            layout="fill"
+            objectFit="contain"
+            placeholder="blur"
+          />
+        </div>
+        <h3>请使用微信扫描二维码开始验证</h3>
+      </div>
+    </div>
+  );
 };
 
 export default PCScreen;
--- pages/faqs.js
+++ pages/faqs.js
@@ -27,7 +27,7 @@ const Faqs = () => {
         />
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1.0, maximum-scale=1.0,user-scalable=0"
+          content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
